6d3a0d05fe342cbea595e60a0dcfb9a048d08790
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-05-05 19:12:38 +0700

    init main windows

---
+++ src/main/java/oop/if2210_tb2_sc4/MainMenu.java
@@ -0,0 +1,25 @@
+package oop.if2210_tb2_sc4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainMenu extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        primaryStage.centerOnScreen();
+        primaryStage.setTitle("Hello, World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
