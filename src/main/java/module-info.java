ce69f6eb3bd70ea277423c555d2438a49a6c7eba
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-05-05 14:42:40 +0700

    Init project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module oop.if2210_tb2_sc4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens oop.if2210_tb2_sc4 to javafx.fxml;
+    exports oop.if2210_tb2_sc4;
+}
---
+++ src/main/java/oop/if2210_tb2_sc4/HelloController.java
@@ -0,0 +1,14 @@
+package oop.if2210_tb2_sc4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
