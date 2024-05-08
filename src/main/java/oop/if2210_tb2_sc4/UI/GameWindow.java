0a2b90e8f25447907f6dbb377586bc8dfaa1e93e
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2024-05-08 19:22:40 +0700

    fix load

--- src/main/java/oop/if2210_tb2_sc4/UI/GameWindow.java
+++ src/main/java/oop/if2210_tb2_sc4/UI/GameWindow.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class GameWindow extends Application {
 
@@ -17,7 +18,7 @@ public class GameWindow extends Application {
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("GameWindow.fxml"));
         Scene scene = new Scene(loader.load(), 1024, 720);
-        scene.getStylesheets().add(getClass().getResource("GameWindow.css").toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("GameWindow.css")).toExternalForm());
         primaryStage.setTitle("Game Window");
         primaryStage.setScene(scene);
         primaryStage.setMinWidth(1024);
