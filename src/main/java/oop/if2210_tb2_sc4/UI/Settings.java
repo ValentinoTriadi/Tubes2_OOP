11d982e9eda89901ef31ff477e8cfcce5a881fa4
Loxenary <110291644+Loxenary@users.noreply.github.com>
2024-05-23 05:34:18 +0700

    Feat : Update Settings Menu

--- src/main/java/oop/if2210_tb2_sc4/UI/Settings.java
+++ src/main/java/oop/if2210_tb2_sc4/UI/Settings.java
@@ -6,6 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import javax.swing.event.ChangeListener;
 
 public class Settings {
 
@@ -19,24 +22,8 @@ public class Settings {
     public AnchorPane Root;
 
     public Settings() {
+
     }
-
-    public void initialize() {
-        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            double percentage = newValue.doubleValue() / volumeSlider.getMax();
-            String style = String.format("-fx-background-color: linear-gradient(to right, #4caf50 %f%%, #e0e0e0 %f%%);",
-                    percentage * 100, percentage * 100);
-            volumeSlider.lookup(".track .fill").setStyle(style);
-        });
-
-        sfxSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-            double percentage = newValue.doubleValue() / sfxSlider.getMax();
-            String style = String.format("-fx-background-color: linear-gradient(to right, #4caf50 %f%%, #e0e0e0 %f%%);",
-                    percentage * 100, percentage * 100);
-            sfxSlider.lookup(".track .fill").setStyle(style);
-        });
-    }
-
 
     public void CloseSettings(ActionEvent actionEvent) {
         Root.setVisible(false);
